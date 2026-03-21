6492de776573669a0f8cf25f6a7f10bd35394e74
yaman-khatib <it.yaman.khateeb@gmail.com>
2026-03-21 16:04:55 +0300

    Add Error codes class

---
+++ Library.BL/Results/ErrorCodes.cs
@@ -0,0 +1,13 @@
+namespace Library.BL.Results;
+
+public static class ErrorCodes
+{
+    public const string ValidationError = "ValidationError";
+    public const string NotFound = "NotFound";
+    public const string NotAvailable = "NotAvailable";
+    public const string AlreadyBorrowed = "AlreadyBorrowed";
+    public const string AlreadyReturned = "AlreadyReturned";
+    public const string Conflict = "Conflict";
+    public const string DbError = "DbError";
+}
+
