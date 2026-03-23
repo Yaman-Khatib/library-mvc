336cc39a31e54f106614d4852f237e6541668726
yaman-khatib <it.yaman.khateeb@gmail.com>
2026-03-23 16:21:29 +0300

    setup tailwind and project settings

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Library.Web/Views/**/*.cshtml",
+    "./Library.Web/Controllers/**/*.cs",
+    "./Library.Web/Models/**/*.cs",
+    "./Library.Web/**/*.cshtml",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
